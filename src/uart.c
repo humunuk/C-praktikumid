dad8b6b650e432218ab5c3374e7f1c09f0ba984e
humunuk <siimkallari@gmail.com>
2016-11-19 15:24:03 +0200

    lab02 finished

--- src/uart.c
+++ src/uart.c
@@ -1,9 +1,72 @@
-void uart_putchar(char c, FILE *stream);
-char uart_getchar(FILE *stream);
+#include <avr/io.h>
+#include <stdio.h>
 
-void uart_init(void);
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
 
-/* http://www.ermicro.com/blog/?p=325 */
+#ifndef BAUD
+#define BAUD 9600
+#endif
+#include <util/setbaud.h>
 
-FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
-FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+/* http://www.cs.mun.ca/~rod/Winter2007/4723/notes/serial/serial.html */
+
+void uart4_init(void)
+{
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+#if USE_2X
+    UCSR0A |= _BV(U2X0);
+#else
+    UCSR0A &= ~(_BV(U2X0));
+#endif
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); /* 8-bit data */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);   /* Enable TX */
+}
+
+void myuart_initialize(void)
+{
+    UBRR3H = UBRRH_VALUE;
+    UBRR3L = UBRRL_VALUE;
+#if USE_2X
+    UCSR3A |= _BV(U2X3);
+#else
+    UCSR3A &= ~(_BV(U2X3));
+#endif
+    UCSR3C = _BV(UCSZ31) | _BV(UCSZ30); /* 8-bit data */
+    UCSR3B = _BV(TXEN3);   /* Enable TX */
+}
+
+int uart4_putchar(char c, FILE *stream)
+{
+    (void) stream;
+
+    if (c == '\n') {
+        uart4_putchar('\r', stream);
+    }
+
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = c;
+    return 0;
+}
+
+int myuart_putchar(char c, FILE *stream)
+{
+    (void) stream;
+
+    if (c == '\n') {
+        myuart_putchar('\r', stream);
+    }
+
+    loop_until_bit_is_set(UCSR3A, UDRE3);
+    UDR3 = c;
+    return 0;
+}
+
+int uart4_getchar(FILE *stream)
+{
+    (void) stream;
+    loop_until_bit_is_set(UCSR0A, RXC0);
+    return UDR0;
+}
--- src/uart.h
+++ src/uart.h
@@ -1,9 +1,12 @@
-void uart_putchar(char c, FILE *stream);
-char uart_getchar(FILE *stream);
+int uart4_putchar(char c, FILE *stream);
+int uart4_getchar(FILE *stream);
 
-void uart_init(void);
+void myuart_initialize(void);
+int myuart_putchar(char c, FILE *stream);
+
+void uart4_init(void);
 
 /* http://www.ermicro.com/blog/?p=325 */
 
-FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
-FILE uart_input = FDEV_SETUP_STREAM(NULL, uart_getchar, _FDEV_SETUP_READ);
+FILE uart4_io = FDEV_SETUP_STREAM(uart4_putchar, uart4_getchar, _FDEV_SETUP_RW);
+FILE myuart_out = FDEV_SETUP_STREAM(myuart_putchar, NULL, _FDEV_SETUP_WRITE);
